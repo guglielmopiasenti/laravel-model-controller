353746163fb558b09747b728529002e24b17e163
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-08-22 17:35:09 +0200

    changing style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,25 @@
 @extends('layouts.main')
 
 @section('main')
-<div class="container">
-
-  <h1>The biggest movies repository </h1>
+  <h1 class="text-center mb-4">The biggest movies repository</h1>
   <section id="movies">
-    <div class="row row-cols-4">
+    <div class="row row-cols-1 row-cols-md-4 g-4">
       @forelse ($movies as $movie)
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">{{$movie['original_title']}}</h5>
-          <h6 class="card-subtitle mb-2 text-body-secondary">{{$movie['vote']}}</h6>
-          <p class="card-text">{{$movie['nationality']}}</p>
-          <p class="card-text">{{$movie['date']}}</p>
+      <div class="col">
+        <div class="card h-100 movie-card">
+          <div class="card-body">
+            <h5 class="card-title">{{$movie['original_title']}}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{{$movie['vote']}}</h6>
+            <p class="card-text">{{$movie['nationality']}}</p>
+            <p class="card-text">{{$movie['date']}}</p>
+          </div>
         </div>
       </div>
       @empty
-          <h3>Sorry, there are no movies</h3>
+        <div class="col">
+          <h3 class="text-center">Sorry, there are no movies</h3>
+        </div>
       @endforelse
     </div>
   </section>
-</div>
-
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,16 +10,39 @@
   @yield('cdns')
   {{-- styles --}}
   @vite('resources/js/app.js')
-</head>
-<body>
-  <header>
-
-  </header>
-  <main>
-    @yield('main')
-  </main>
-  <footer>
-    
-  </footer>
-</body>
+  <body class="bg-dark text-white">
+    <header class="header">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">MovieDB</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Genres</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">New Releases</a>
+              </li>
+            </ul>
+            <form class="d-flex" role="search">
+              <input class="form-control me-2 bg-dark text-white" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-light" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <main class="container my-4">
+      @yield('main')
+    </main>
+    <footer class="text-center text-white mt-4 footer">
+      <p>&copy; 2023 MovieDB. All rights reserved.</p>
+    </footer>
+  </body>
 </html>
